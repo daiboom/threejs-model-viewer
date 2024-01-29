3dcf09e02593f82107c208b04bb74232ec7573f6
daiboom <dbchoi@aboutfishing.kr>
2024-01-29 14:31:41 +0900

    formatter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
+import { setupCounter } from './counter.ts'
 import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -13,7 +13,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     </a>
     <h1>Vite + TypeScript</h1>
     <div class="card">
-      <button id="counter" type="button"></button>
+        <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
       Click on the Vite and TypeScript logos to learn more
